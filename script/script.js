d8405bb487c087a813a478107f26bb7f9d2998c6
Alexandr <aleksandr.lonski.86@gmail.com>
2020-11-04 17:13:09 +0300

    add №19

--- script/script.js
+++ script/script.js
@@ -42,5 +42,67 @@ window.addEventListener('DOMContentLoaded', function() {
  }  
   countTimer('10 november 2020');
   
- 
+
+  //  menu
+  const toggleMenu = () => {
+     
+     const btnMenu = document.querySelector('.menu'),
+           menu = document.querySelector('menu'),
+           closeBtn = document.querySelector('.close-btn'),
+           menuItems = menu.querySelectorAll('ul>li');
+
+     const handlermenu = () => {
+       menu.classList.toggle('active-menu');
+     };      
+     btnMenu.addEventListener('click', handlermenu);
+     closeBtn.addEventListener('click', handlermenu);
+     menuItems.forEach((elem) => elem.addEventListener('click', handlermenu));
+  };
+
+  toggleMenu();
+
+  // popup
+  const togglePopup = () => {
+    const popup = document.querySelector('.popup'),
+          popupBtn = document.querySelectorAll('.popup-btn'),
+          popupClose = document.querySelector('.popup-close');
+    let   popupContent = document.querySelector('.popup-content'),
+          count =100;
+          let popupRight = function() {
+              popup.style.display = 'block';
+               count--;
+               if(screen.width >= 768 && count > 10){
+                 popupContent.style.top = count +'%';
+                 setTimeout(popupRight, 10);  
+               } 
+            };
+          popupBtn.forEach((elem) => {
+            elem.addEventListener('click', popupRight );
+           });
+        
+          popupClose.addEventListener('click', () => {
+             popup.style.display = 'none';
+             count = 100;
+          }); 
+  };
+  
+  togglePopup();
+  console.log(screen.width);
+
+  // scroll
+
+
+  const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (elem) {
+    elem.preventDefault();
+    const blockID = anchor.getAttribute('href').substr(1);
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+}
+
 });
