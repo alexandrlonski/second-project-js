fb3edf0282c70e9e844a36150fe431717b5f4de0
Alexandr <aleksandr.lonski.86@gmail.com>
2020-11-16 00:17:59 +0300

    some change

--- script/script.js
+++ script/script.js
@@ -363,7 +363,85 @@ const sendForm = () => {
          inputName = document.querySelectorAll('[name=user_name], [name=user_message]'),
          statusMessage = document.createElement('div');
 
-
+    const applyStyle = () => {
+    const style = document.createElement('style');
+    style.setAttribute('type', 'text/css');
+    style.textContent = `
+      .sk-wave {
+        width: 6em;
+        height: 4em;
+        margin: auto;
+        text-align: center;
+        font-size: 1em;
+      }
+      
+      .sk-wave .sk-rect {
+        background-color: #337ab7;
+        height: 100%;
+        width: .5em;
+        display: inline-block;
+        -webkit-animation: sk-wave-stretch-delay 1.2s infinite ease-in-out;
+        animation: sk-wave-stretch-delay 1.2s infinite ease-in-out;
+      }
+      
+      .sk-wave .sk-rect-1 {
+        -webkit-animation-delay: -1.2s;
+        animation-delay: -1.2s;
+      }
+      
+      .sk-wave .sk-rect-2 {
+        -webkit-animation-delay: -1.1s;
+        animation-delay: -1.1s;
+      }
+      
+      .sk-wave .sk-rect-3 {
+        -webkit-animation-delay: -1s;
+        animation-delay: -1s;
+      }
+      
+      .sk-wave .sk-rect-4 {
+        -webkit-animation-delay: -0.9s;
+        animation-delay: -0.9s;
+      }
+      
+      .sk-wave .sk-rect-5 {
+        -webkit-animation-delay: -0.8s;
+        animation-delay: -0.8s;
+      }
+      
+      @-webkit-keyframes sk-wave-stretch-delay {
+      
+        0%,
+        40%,
+        100% {
+          -webkit-transform: scaleY(0.4);
+          transform: scaleY(0.4);
+        }
+      
+        20% {
+          -webkit-transform: scaleY(1);
+          transform: scaleY(1);
+        }
+      }
+      
+      @keyframes sk-wave-stretch-delay {
+      
+        0%,
+        40%,
+        100% {
+          -webkit-transform: scaleY(0.4);
+          transform: scaleY(0.4);
+        }
+      
+        20% {
+          -webkit-transform: scaleY(1);
+          transform: scaleY(1);
+        }
+      }
+    `;
+    document.head.appendChild(style);
+  };
+  
          inputName.forEach((elem) => {
              elem.addEventListener('input', () => {
                elem.value = elem.value.replace(/[^а-яё\s]/ig, '');
@@ -387,6 +465,7 @@ const sendForm = () => {
                                          <div class='sk-rect sk-rect-4'></div>
                                          <div class='sk-rect sk-rect-5'></div>
                                        </div>`;
+            applyStyle();                           
             const formData = new FormData(form);
             let body = {};
             // for(let val of formData.entries()){
